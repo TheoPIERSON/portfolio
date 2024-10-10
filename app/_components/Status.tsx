0ae419bc06d0034f1efdc9a3d10ce9145b305ce0
Theo Pierson <thheopierson@gmail.com>
2024-10-10 16:06:06 +0200

    feat: add Do Bronx MMA to status

--- app/_components/Status.tsx
+++ app/_components/Status.tsx
@@ -8,6 +8,7 @@ import WatIRead from "@/app/assets/images/watiread.png";
 import RoseNail from "@/app/assets/images/rosenail.png";
 import Mighty from "@/app/assets/images/mighty-coaching.png";
 import ChefPro from "@/app/assets/images/chef-pro.png";
+import DoBronx from "@/app/assets/images/logo copy.png";
 import { cn } from "@/lib/utils";
 import { ComponentPropsWithRef } from "react";
 
@@ -24,11 +25,22 @@ const Code = ({ className, ...props }: ComponentPropsWithRef<"span">) => {
 };
 export const Status = () => {
   return (
-    <Section className="grid grid-cols-1 md:grid-cols-2 gap-10">
+    <Section className="md:mt-32 grid grid-cols-1 md:grid-cols-2 gap-10">
+      <div>
+        <a className="block" href="https://do-bronx-mma.vercel.app/">
+          <Card className="w-full p-4 flex items-center gap-2">
+            <Image className="w-10 h-10 object-contain bg-slate-50 rounded-xl" alt="Chef-pro-360 logo" src={DoBronx} />
+            <span>Do Bronx MMA</span>
+          </Card>
+        </a>
+        <h1 className="mt-2">
+          <Code>NuxtJs</Code>
+        </h1>
+      </div>
       <div>
         <a className="block" href="https://chef-pro-360.vercel.app/">
           <Card className="w-full p-4 flex items-center gap-2">
-            <Image className="w-10" alt="Chef-pro-360 logo" src={ChefPro} />
+            <Image className="w-10 h-10 " alt="Chef-pro-360 logo" src={ChefPro} />
             <span>Chef Pro 360</span>
           </Card>
         </a>
@@ -39,7 +51,7 @@ export const Status = () => {
       <div>
         <a className="block" href="https://onyx-institut.com/">
           <Card className="w-full p-4 flex items-center gap-2">
-            <Image className="w-10" alt="Onyx Institut logo" src={OnyxLogo} />
+            <Image className="w-10 h-10 " alt="Onyx Institut logo" src={OnyxLogo} />
             <span>Onyx Institut</span>
           </Card>
         </a>
@@ -52,7 +64,7 @@ export const Status = () => {
       <div>
         <a className="block" href="https://mighty-coaching.vercel.app/">
           <Card className="w-full p-4 flex items-center gap-2">
-            <Image className="w-10" alt="Mighty logo" src={Mighty} />
+            <Image className="w-10 h-10 " alt="Mighty logo" src={Mighty} />
             <span> Mighty Coaching</span>
           </Card>
         </a>
@@ -63,7 +75,7 @@ export const Status = () => {
       <div>
         <a className="block" href="https://dad-s-joke-nu.vercel.app/">
           <Card className="w-full p-4 flex items-center gap-2">
-            <Image className="w-10" alt="Dad's joke logo" src={DadJokeLogo} />
+            <Image className="w-10 h-10 " alt="Dad's joke logo" src={DadJokeLogo} />
             <span>Dad Joke randomizer</span>
           </Card>
         </a>
@@ -75,7 +87,7 @@ export const Status = () => {
       <div>
         <a className="block" href="https://wat-i-read.vercel.app/">
           <Card className="w-full p-4 flex items-center gap-2">
-            <Image className="w-10 bg-slate-200 rounded" alt="WAT I READ logo" src={WatIRead} />
+            <Image className="w-10 h-10  bg-slate-200 rounded" alt="WAT I READ logo" src={WatIRead} />
             <span> Wat I Read</span>
           </Card>
         </a>
