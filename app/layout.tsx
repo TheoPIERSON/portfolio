0b1155f6377a61bcb8fefea5b3408bafff474dc0
Theo Pierson <thheopierson@gmail.com>
2024-10-10 16:27:44 +0200

    feat: change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <link rel="icon" href="icon.png" />
+      </head>
       <body
         className={cn(
           GeistSans.variable,
